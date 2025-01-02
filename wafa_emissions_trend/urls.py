b00ebe4d1cc64cc357178a6268cc4e9fb8487e02
wafa-meddeb <wafa.meddeb@esprit.tn>
2025-01-02 22:16:51 +0100

    implementing the prediction logic + updating the form html

--- wafa_emissions_trend/urls.py
+++ wafa_emissions_trend/urls.py
@@ -1,8 +1,6 @@
-
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('prediction_form', views.predict, name='predict_test_form'),
+  path('prediction_form', views.predict, name='prediction_form'),
 ]
