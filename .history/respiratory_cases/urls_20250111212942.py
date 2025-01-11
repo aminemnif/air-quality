c3ff09c2606d170b004a1cebf4c2110ec31ee1c6
Bechir MEJRI <bechir.mejri@esprit.tn>
2025-01-11 22:16:03 +0100

    resperatory_cases/clutering: hejer path commented, taha views commented

--- .history/respiratory_cases/urls_20250111212942.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import predict_view,clustering_RS
-
-urlpatterns = [
-  path('', predict_view, name='predict_RS'),
-  path('clustering', clustering_RS, name='clustering_RS'),
-  path('associationRule', associationRule_RS, name='clustering_RS'),
-]
